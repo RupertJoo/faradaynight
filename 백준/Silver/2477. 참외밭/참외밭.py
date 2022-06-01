be3eaca9d968b9f42e41de528a0436368282e8c0
RupertJoo <92567198+RupertJoo@users.noreply.github.com>
2022-06-01 14:06:05 +0900

    [Silver III] Title: 참외밭, Time: 84 ms, Memory: 32460 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,4 +1,3 @@
-
 def boj_2477():
     from sys import stdin
     from collections import deque
@@ -13,36 +12,22 @@ def boj_2477():
         list_c[list_v[j][0]-1]+=1
 
     dq=deque(list_v)
+
     if list_c==[1,2,1,2]:
-        while dq[0][0]!=1:
-            dq.rotate(-1)
-        if dq[1][0]==3:
-            area=dq[0][1]*dq[1][1]-dq[3][1]*dq[4][1]
-        else: 
-            area=dq[0][1]*dq[-1][1]-dq[2][1]*dq[3][1]
-    
+        l1,l2=1,3
     elif list_c==[1,2,2,1]:
-        while dq[0][0]!=1:
-            dq.rotate(-1)
-        if dq[1][0]==4:
-            area=dq[0][1]*dq[1][1]-dq[3][1]*dq[4][1]
-        else: area=dq[0][1]*dq[-1][1]-dq[2][1]*dq[3][1] 
-    
+        l1,l2=1,4
     elif list_c==[2,1,1,2]:
-        while dq[0][0]!=2:
-            dq.rotate(-1)
-        if dq[1][0]==3:
-            area=dq[0][1]*dq[1][1]-dq[3][1]*dq[4][1]
-        else: area=dq[0][1]*dq[-1][1]-dq[2][1]*dq[3][1] 
-    
+        l1,l2=2,3
     elif list_c==[2,1,2,1]:
-        while dq[0][0]!=2:
-            dq.rotate(-1)
-        if dq[1][0]==4:
-            area=dq[0][1]*dq[1][1]-dq[3][1]*dq[4][1]
-        else: area=dq[0][1]*dq[-1][1]-dq[2][1]*dq[3][1]    
-    
+        l1,l2=2,4
+
+    while dq[0][0]!=l1:
+        dq.rotate(-1)
+    if dq[1][0]==l2:
+        area=dq[0][1]*dq[1][1]-dq[3][1]*dq[4][1]
+    else: area=dq[0][1]*dq[-1][1]-dq[2][1]*dq[3][1]
+
     n_melon=density*area
     print(n_melon)
-
 boj_2477()
